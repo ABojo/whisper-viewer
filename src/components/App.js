f2cb3d299950c142a8652b4b68ba984fde3caf35
ABojo <alex.bojorquez2598@gmail.com>
2021-06-22 15:51:59 -0400

    Created a loader component and have it display based on whether or not a request is still pending

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import '../css/styles.css';
 import PostsGrid from './PostsGrid';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import Navbar from './Navbar';
 import formatDate from '../utils/formatDate';
 import API from '../utils/API';
+import Loader from './Loader';
 
 function App() {
   const [posts, setPosts] = useState('');
   const [updateTime, setUpdateTime] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const getPosts = async () => {
+    setIsLoading(true);
     const json = await API.getLatest();
     setPosts(json.data.latestPosts);
     setUpdateTime(`Last updated at ${formatDate(new Date())}`);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -23,10 +27,16 @@ function App() {
     <div className="bg-white min-h-screen">
       <div className="w-11/12 max-w-screen-md mx-auto">
         <Navbar getPosts={getPosts} />
-        <p className="text-lg mb-6 text-gray-400 text-center">
-          {updateTime && updateTime}
-        </p>
-        <PostsGrid posts={posts} />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <Fragment>
+            <p className="text-lg mb-6 text-gray-400 text-center">
+              {updateTime && updateTime}
+            </p>
+            <PostsGrid posts={posts} />
+          </Fragment>
+        )}
       </div>
     </div>
   );
---
+++ src/components/Loader.js
@@ -0,0 +1,5 @@
+function Loader() {
+  return <div className="loader text-purple-500"></div>;
+}
+
+export default Loader;
