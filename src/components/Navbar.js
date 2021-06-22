a4a84a2f56d65734ff044c7c36f2d35a0f129f47
ABojo <alex.bojorquez2598@gmail.com>
2021-06-22 15:13:06 -0400

    Added refresh button to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 function Navbar() {
   return (
     <div className="flex justify-between items-baseline py-12">
-      <h1 className="text-purple-500 font-bold text-2xl">Whisper's Latest</h1>
+      <h1 className="text-purple-500 text-2xl">Whisper's Latest</h1>
+      <button className="bg-purple-500 rounded p-2 hover:bg-purple-700 transition duration-200">
+        <i class="fas fa-sync-alt text-white text-xl"></i>
+      </button>
     </div>
   );
 }
