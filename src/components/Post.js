deedef9b977a1fa0e2d496f9f02990abfea80075
ABojo <alex.bojorquez2598@gmail.com>
2021-06-22 19:26:45 -0400

    Rebuilt post design and added location underneath the username

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,12 +2,15 @@ function Post(props) {
   const { post } = props;
 
   return (
-    <div className="py-5 px-5 bg-purple-500 shadow rounded">
-      <h1 className="text-white font-bold text-center mb-2 text-md">
-        <i class="fas fa-user mr-3"></i>
-        {post.nickname}
-      </h1>
-      <img src={post.url} alt={post.text} className="rounded mx-auto mb-2" />
+    <div className="shadow">
+      <div className="bg-purple-500 p-5 rounded-t">
+        <p className="text-white font-bold mb-2">
+          <i class="fas fa-user mr-2"></i>
+          {post.nickname}
+        </p>
+        <p className="text-white text-sm">{post.location}</p>
+      </div>
+      <img src={post.url} alt={post.text} className="rounded-b mx-auto" />
     </div>
   );
 }
