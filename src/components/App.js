56be6ce48620580927cd81394347e59e6a1b2f00
ABojo <alex.bojorquez2598@gmail.com>
2021-06-22 14:39:38 -0400

    Built a basic navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import '../css/styles.css';
 import PostsGrid from './PostsGrid';
 import { Fragment } from 'react';
-
+import Navbar from './Navbar';
 function App() {
   return (
-    <div className="w-11/12 max-w-screen-md mx-auto">
-      <PostsGrid />
+    <div className="bg-gray-50 min-h-screen">
+      <div className="w-11/12 max-w-screen-md mx-auto">
+        <Navbar />
+        <PostsGrid />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div className="flex justify-between items-baseline py-12">
+      <h1 className="text-purple-500 font-bold text-2xl">Whisper's Latest</h1>
+    </div>
+  );
+}
+
+export default Navbar;
