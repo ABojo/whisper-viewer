1ac645938cde1b9d8b3c1cb8b32ff62b70f7f1a1
ABojo <alex.bojorquez2598@gmail.com>
2021-06-23 19:40:57 -0400

    added a get owner method to the API module and added a down arrow to open the more details portion of the cards

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,13 +3,19 @@ function Post(props) {
 
   return (
     <div className="shadow">
-      <div className="bg-purple-500 p-5 rounded-t">
-        <p className="text-white font-bold mb-2">
-          <i className="fas fa-user mr-2"></i>
-          {post.nickname}
-        </p>
-        <p className="text-white text-sm">{post.location}</p>
+      <div className="bg-purple-500 p-5 rounded-t flex justify-between items-end">
+        <div>
+          <p className="text-white font-bold mb-2">
+            <i className="fas fa-user mr-2"></i>
+            {post.nickname}
+          </p>
+          <p className="text-white text-sm">{post.location}</p>
+        </div>
+        <button>
+          <i class="text-white text-lg fas fa-arrow-down"></i>
+        </button>
       </div>
+
       <img src={post.url} alt={post.text} className="rounded-b mx-auto" />
     </div>
   );
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,13 +1,23 @@
 const API = (() => {
-	const getLatest = async () => {
-		const response = await fetch('https://whisper-latest.herokuapp.com/api/posts');
-		const json = await response.json();
+  const getLatest = async () => {
+    const response = await fetch(
+      'https://whisper-latest.herokuapp.com/api/posts'
+    );
+    const json = await response.json();
 
-		return json;
-	};
+    return json;
+  };
 
-	return {getLatest};
+  const getOwner = async (wid) => {
+    const response = await fetch(
+      `https://whisper-latest.herokuapp.com/api/posts/${wid}`
+    );
+    const json = await response.json();
+
+    return json;
+  };
+
+  return { getLatest, getOwner };
 })();
-
 
 export default API;
