3b117ea91bd62c0e801a51019b792bab199ee2f1
ABojo <alex.bojorquez2598@gmail.com>
2021-06-25 17:55:37 -0400

    Added the functionality to give users the ability to search for whispers by keyword

--- src/components/App.js
+++ src/components/App.js
@@ -9,20 +9,28 @@ import formatDate from '../utils/formatDate';
 import API from '../utils/API';
 
 function App() {
-  const [searchValue, setSearchValue] = useState('');
+  //holds the string currently in the serach input field
+  const [searchField, setSearchField] = useState('');
+
+  //holds the string that was in the input box the last time the search button was pressed
+  const [activeSearch, setActiveSearch] = useState('');
+
   const [scrollId, setScrollId] = useState('');
   const [posts, setPosts] = useState('');
   const [updateTime, setUpdateTime] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
+  const fetchPosts = async () => {
+    if (activeSearch) return await API.searchPosts(activeSearch);
+
+    return await API.getLatest();
+  };
+
   const getLatestPosts = async () => {
     setIsLoading(true);
-
-    const json = await API.getLatest();
+    const json = await fetchPosts();
     setPosts(json.data.latestPosts);
     setScrollId(json.data.scrollId);
-    setUpdateTime(`Last updated at ${formatDate(new Date())}`);
-
     setIsLoading(false);
   };
 
@@ -32,19 +40,23 @@ function App() {
     setScrollId(json.data.scrollId);
   };
 
+  const handleSearch = async () => {
+    setActiveSearch(searchField);
+  };
+
   useEffect(() => {
     getLatestPosts();
-  }, []);
+  }, [activeSearch]);
 
   return (
     <div className="bg-white min-h-screen">
       <div className="w-11/12 max-w-screen-lg mx-auto">
         <Navbar getPosts={getLatestPosts} isLoading={isLoading} />
+        <SearchField setSearchField={setSearchField} onSearch={handleSearch} />
         {isLoading ? (
           <Loader classes="text-purple-500" />
         ) : (
           <Fragment>
-            <SearchField setSearchValue={setSearchValue} />
             <p className="text-lg mb-6 text-gray-400 text-center">
               {updateTime && updateTime}
             </p>
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,14 +1,17 @@
 function SearchField(props) {
-  const { setSearchValue } = props;
+  const { setSearchField, onSearch } = props;
 
   return (
     <div className="w-full max-w-sm mx-auto mb-12 relative border border-gray-100 rounded">
       <input
-        onChange={(e) => setSearchValue(e.currentTarget.value)}
+        onChange={(e) => setSearchField(e.currentTarget.value)}
         placeholder="Enter a keyword"
         className="p-3 pr-24 bg-purple-100 rounded  block w-full "
       />
-      <button className="bg-white p-3 text-purple-500 absolute rounded-r top-0 right-0 font-bold hover:bg-gray-200 transition duration-200">
+      <button
+        onClick={onSearch}
+        className="bg-white p-3 text-purple-500 absolute rounded-r top-0 right-0 font-bold hover:bg-gray-200 transition duration-200"
+      >
         Search
       </button>
     </div>
